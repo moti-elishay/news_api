2e91c1637ec8e7c000b7af7db4c1eec4337351f7
moti-elishay <moti.elshay@gmail.com>
2018-03-21 14:11:40 +0200

    Some changes from Part 2

--- app/src/main/java/com/example/dell/newsapi/Adapter/ListSourceAdapter.java
+++ app/src/main/java/com/example/dell/newsapi/Adapter/ListSourceAdapter.java
@@ -1,6 +1,7 @@
 package com.example.dell.newsapi.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,15 +11,11 @@ import android.widget.TextView;
 import com.example.dell.newsapi.Common.Common;
 import com.example.dell.newsapi.Interface.IconBetterIdeaService;
 import com.example.dell.newsapi.Interface.ItemClickListener;
-import com.example.dell.newsapi.Model.IconBetterIdea;
+import com.example.dell.newsapi.ListNews;
 import com.example.dell.newsapi.Model.WebSite;
 import com.example.dell.newsapi.R;
-import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 /**
  * Created by DELL on 3/19/2018.
@@ -78,18 +75,22 @@ public class ListSourceAdapter extends RecyclerView.Adapter<ListSourceViewHolder
 
     @Override
     public void onBindViewHolder(final ListSourceViewHolder holder, int position) {
-
+        /*
         StringBuilder iconBetterAPI = new StringBuilder("http://faviconget.herokuapp.com/");
         iconBetterAPI.append(webSite.getSources().get(position).getUrl());
-
         mService.getIconUrl(iconBetterAPI.toString())
                 .enqueue(new Callback<IconBetterIdea>() {
                     @Override
                     public void onResponse(Call<IconBetterIdea> call, Response<IconBetterIdea> response) {
-                        if (response.body().getIcons().size() > 0) {
-                            Picasso.with(context)
-                                    .load(response.body().getIcons().get(0).getUrl())
-                                    .into(holder.source_image);
+                        try {
+                            if (response.body().getIcons().size() > 0) {
+                                Picasso.with(context)
+                                        .load(response.body().getIcons().get(0).getUrl())
+                                        .into(holder.source_image);
+                            }
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
                         }
 
                     }
@@ -99,6 +100,8 @@ public class ListSourceAdapter extends RecyclerView.Adapter<ListSourceViewHolder
 
                     }
                 });
+        */
+
 
         holder.source_title.setText(webSite.getSources().get(position).getName());
 
@@ -106,6 +109,12 @@ public class ListSourceAdapter extends RecyclerView.Adapter<ListSourceViewHolder
             @Override
             public void onClick(View view, int position, boolean isLongClick) {
                 //Implement on part 2
+
+                Intent intent = new Intent(context, ListNews.class);
+                intent.putExtra("source", webSite.getSources().get(position).getId());
+                context.startActivity(intent);
+
+
             }
         });
 
